874dd099ed91d2d092b73979058b3fa1ca5b477d
vcorreal <victor.correal@pharmacelera.com>
2021-08-16 14:05:54 +0200

    Add documentation

--- docs/type-search-index.js
+++ docs/type-search-index.js
@@ -1,1 +1,1 @@
-typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"WHOPAYS.Domain","l":"DomainObject"},{"p":"WHOPAYS.Domain","l":"Group"},{"p":"WHOPAYS.Domain","l":"Item"},{"p":"WHOPAYS.Domain","l":"Person"},{"p":"WHOPAYS.Domain","l":"PersonAdmin"},{"p":"WHOPAYS.Domain","l":"PersonUser"},{"p":"WHOPAYS.Domain","l":"ShoppingTickets"},{"p":"WHOPAYS.Domain","l":"xDomainController"},{"p":"WHOPAYS.Domain","l":"xUserController"}]
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"WHOPAYS","l":"inout"},{"p":"WHOPAYS","l":"LOG"}]
